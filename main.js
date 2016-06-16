50b3406920b56d7d8decfd2326bcb2f6be9572eb
Michael Albonetti <malbo24@gmail.com>
2016-06-16 16:43:34 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+   
+    
+    
+    
+    
+});
+
+// check if modal active
+    function activeModal(){
+        
+    }
+
+// sign in
+    
+    function signIn(){
+      alert("signing in");  
+    };
+    
+    
+    // register
+    
+    function register(){
+        alert("registering");
+    };
+    
+    
+    // close modal
+    
+    function closeModal(){
+      alert("close modal");  
+    };
