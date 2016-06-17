0208a9c899415db38718176e7ef00972b72c42c4
Michael Albonetti <malbo24@gmail.com>
2016-06-17 18:31:10 -0500

    update footer, refine modal functions, refactor js

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ $(document).ready(function(){
     
     //when the login or register modal are launched, the background will be blurred to provide contrast between it and the modal 
     
-    var isBlurred = null;
     var loginActive = true;
+    var registerActive = true;
     var bodyText = $(".bodyText");
     var loginDiv = $("#loginDiv");
+    var registerDiv = $("#registerDiv");
+    
+    var smallFooterText = '<p class="col-xs-6 col-xs-offset-3">Click here to <a class="minSignIn"> Sign In</a>, or to <a class="minRegister"> Register</a>.  </p>';
+    
+    var largeFooterText = 'To comment and save, sign in or create an account.';
+    
     
     
     // blur function
@@ -28,62 +34,120 @@ $(document).ready(function(){
     // blur background 
     
     function blurBackground(){
-        
-        if(isBlurred){
-            isBlurred = false;
-            //$(bodyText).animate({opacity: 1}, 200);
-            blurElement(bodyText, 0, 1);
-        }else{
-            isBlurred = true;
-           // $(bodyText).animate({opacity: ".3"}, 200);
-            blurElement(bodyText, 2, '.8');
-        }
-        
-    };
+        blurElement(bodyText, 1, ".5");
+    }
+    
+    function unblurBackground(){
+        blurElement(bodyText, 0, 1);
+    }
     
     
-    // the sign in, register, and close modal buttons. 
-
-    // sign in
-
+    // modal show/hide
     
-    
-    $(".signIn").on('click', function(){
-        
-        if(loginActive){
-            blurBackground();
-            $(loginDiv).animate({
+    function showDiv(element){
+        $(element).animate({
                 opacity: 1,
                 top: "10%"
             }, 100).css("visibility", "visible");
-            loginActive = null;
-        } else {
-            blurBackground();
-            $(loginDiv).animate({
+    }
+    
+    function hideDiv(element){
+        $(element).animate({
                 opacity: 0,
-                top: "25%",
-                visibility: "hidden"
-            }, 100);
+                top: "25%"
+            }, 100).css("visibility", "hidden");
+    }
+    
+    function loginModal(){
+        if(loginActive){
+            blurBackground();
+            showDiv(loginDiv);
+            hideDiv(registerDiv);
+            loginActive = null;
+            registerActive = true;
+        } else {
+            unblurBackground();
+            hideDiv(loginDiv);
             loginActive = true;
+            registerActive = true;
         }
+    };
+    
+    function registerModal(){
+        if(registerActive){
+            blurBackground();
+            showDiv(registerDiv);
+            hideDiv(loginDiv);
+            registerActive = null;
+            loginActive = true;
+        } else {
+            unblurBackground();
+            hideDiv(registerDiv);
+            registerActive = true;
+            loginActive = true;
+        } 
+    };
+    
+    
+    // buttons. 
+
+    // sign in
+    $(".signIn").on('click', function(){ loginModal(); });
+    
+    // register  
+    $(".register").on('click', function(){ registerModal(); });
+    
+    
+    // close modal 
+    $(".closeModal").on('click', function(){
+        hideDiv(loginDiv);
+        hideDiv(registerDiv);
+        registerActive = true;
+        loginActive = true;
+        unblurBackground();
+    });
+    
+    
+    // login button for social apps
+    $(".loginButton").on('click', function(){
+      alert($(this).attr('value'));
+    });
+    
+    
+    // minimize the footer
+    $("#closeFooter").on('click', function(){
+        
+       
+        // hide the buttons and text, and then minmize the bar
+        
+        $("#footerDiv button").animate({ opacity: 0}, 300);
+        
+        $("#footerDiv").animate({height: "5%"});
+        
+        
+        // add a line that allows the user to bring the bar back
+        
+        $("#largeFooterText").html(smallFooterText).css("padding-top", "5px");
+        
+        $(".minSignIn").on('click', function(){ 
+            loginModal();
+            $("#footerDiv").animate({height: "10%"});
+            $("#footerDiv button").animate({opacity: 1}, 300);
+            $("#largeFooterText").html(largeFooterText).css("padding-top", "");
+        });
+        
+        $(".minRegister").on('click', function(){ 
+            registerModal(); 
+            $("#footerDiv").animate({height: "10%"});
+            $("#footerDiv button").animate({opacity: 1}, 300);
+            $("#largeFooterText").html(largeFooterText).css("padding-top", "");
+        });
         
     });
     
-    
-    // register
-    
-    $(".register").on('click', function(){
-       // do something 
-    });
-    
-    
-    // close modal
-    
-    $("#closeModal").on('click', function(){
-       // close the modal 
-    });
     
     
     
 });
+
 
