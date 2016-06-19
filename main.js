67b2b3385ce41c5bec172d7e010d3cc1f76fa7b2
Michael Albonetti <malbo24@gmail.com>
2016-06-19 11:08:51 -0500

    update HTML, and Bootstrap layout

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
    
     
-    //when the login or register modal are launched, the background will be blurred to provide contrast between it and the modal 
+    // -- vars 
     
     var loginActive = true;
     var registerActive = true;
@@ -9,13 +9,16 @@ $(document).ready(function(){
     var loginDiv = $("#loginDiv");
     var registerDiv = $("#registerDiv");
     
-    var smallFooterText = '<p class="col-xs-6 col-xs-offset-3">Click here to <a class="minSignIn"> Sign In</a>, or to <a class="minRegister"> Register</a>.  </p>';
     
+    // -- text for footer div, "smallFooterText" is for the collapsed footer
+    var smallFooterText = '<p class="col-xs-6 col-xs-offset-3">Click here to <a class="minSignIn"> Sign In</a>, or to <a class="minRegister"> Register</a>.  </p> <div class="text-right"><a class="reopenModal"><i  id="" class="fa fa-angle-double-up" aria-hidden="true"></i></a><div>';
+    
+    // -- text for footer div, "largeFooterText" is for the expanded footer
     var largeFooterText = 'To comment and save, sign in or create an account.';
     
     
     
-    // blur function
+    // -- blur function for background
     function blurElement(element, size, opacity){            // via http://jsfiddle.net/3F6jm/
         var filterVal = 'blur(' + size + 'px)';
         $(element)
@@ -31,33 +34,34 @@ $(document).ready(function(){
             .css('-o-transition', 'all 0.3s ease-out');
     };
     
-    // blur background 
-    
+    // -- blur background 
     function blurBackground(){
         blurElement(bodyText, 1, ".5");
     }
     
+    // -- unblur background
     function unblurBackground(){
         blurElement(bodyText, 0, 1);
     }
     
     
-    // modal show/hide
-    
+    // -- modal show
     function showDiv(element){
         $(element).animate({
                 opacity: 1,
                 top: "10%"
-            }, 100).css("visibility", "visible");
+            }, 300).css("visibility", "visible");
     }
     
+    // -- modal hide
     function hideDiv(element){
         $(element).animate({
                 opacity: 0,
                 top: "25%"
-            }, 100).css("visibility", "hidden");
+            }, 300).css("visibility", "hidden");
     }
     
+    // -- function called when any "login" button is clicked.
     function loginModal(){
         if(loginActive){
             blurBackground();
@@ -73,6 +77,8 @@ $(document).ready(function(){
         }
     };
     
+    
+    // -- function called when any "register" button is clicked.
     function registerModal(){
         if(registerActive){
             blurBackground();
@@ -89,16 +95,16 @@ $(document).ready(function(){
     };
     
     
-    // buttons. 
+    // -- da... buttons. 
 
-    // sign in
+    // -- sign in
     $(".signIn").on('click', function(){ loginModal(); });
     
-    // register  
+    // -- register  
     $(".register").on('click', function(){ registerModal(); });
     
     
-    // close modal 
+    // -- close modal 
     $(".closeModal").on('click', function(){
         hideDiv(loginDiv);
         hideDiv(registerDiv);
@@ -108,27 +114,25 @@ $(document).ready(function(){
     });
     
     
-    // login button for social apps
+    // -- login button for social apps
     $(".loginButton").on('click', function(){
       alert($(this).attr('value'));
     });
     
     
-    // minimize the footer
+    // -- minimize the footer
     $("#closeFooter").on('click', function(){
         
        
-        // hide the buttons and text, and then minmize the bar
-        
+        // -- hide the buttons and text, and then minmize the bar
         $("#footerDiv button").animate({ opacity: 0}, 300);
-        
         $("#footerDiv").animate({height: "5%"});
         
         
-        // add a line that allows the user to bring the bar back
-        
+        // -- add a line that allows the user to bring the bar back
         $("#largeFooterText").html(smallFooterText).css("padding-top", "5px");
         
+        // -- event listeners for the added "sign in" and "register" buttons
         $(".minSignIn").on('click', function(){ 
             loginModal();
             $("#footerDiv").animate({height: "10%"});
@@ -143,11 +147,17 @@ $(document).ready(function(){
             $("#largeFooterText").html(largeFooterText).css("padding-top", "");
         });
         
-    });
+        $(".reopenModal").on('click', function(){
+            $("#footerDiv").animate({height: "10%"});
+            $("#footerDiv button").animate({opacity: 1}, 300);
+            $("#largeFooterText").html(largeFooterText).css("padding-top", "");
+        });
+        
+    });  // -- end minimize footer event listener
     
     
     
     
-});
+}); // -- end document ready
 
 
